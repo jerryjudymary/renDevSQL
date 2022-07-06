7c60b3e4ab3c6d312662559506572db4efec08ac
engin9803 <mne9691@gmail.com>
2022-07-06 17:24:10 +0900

    no message

--- models/user.js
+++ models/user.js
@@ -15,68 +15,13 @@ module.exports = (sequelize, DataTypes) => {
       );
     }
   }
-  User.init({
-    id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    userId: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-      unique: true
-    },
-    nickname: {
-      type: DataTypes.STRING(30),
-      allowNull: false,
-      unique: true
-    },
-    name: {
-      type: DataTypes.STRING(30),
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING(200),
-      allowNull: false,
-    },
-    passwordCheck: {
-      type: DataTypes.STRING(200),
-      allowNull: true,
-    },
-    phone: {
-      type: DataTypes.STRING(30),
-      allowNull: false,
-    },
-    birth: {
-      type: DataTypes.STRING(30),
-      allowNull: false,
-    },
-    policy: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-    profileImage: {
-      type: DataTypes.STRING(200),
-      allowNull: true,
-    },
-    refreshToken: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    }
-  }, {
-    sequelize,
-    modelName: 'User',
-    tableName: 'user',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
+  User.init(
+    {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
       },
       userId: {
         type: DataTypes.STRING(50),
