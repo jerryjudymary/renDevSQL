587a9db880e57aa03fe659d58c2376485065799e
jerryjudymary <jerryjudymary@gmail.com>
2022-07-07 21:17:29 +0900

    Docs: 면접 예약 API 난수 생성 제외 1차 커밋, 관련 모델 (Application, Resume) 관계 수정

--- routes/applications.js
+++ routes/applications.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 const { Project, ProjectSkill, ProjectPhoto, Application, Resume, sequelize } = require("../models");
-// 유효성 검사 관련 import 추가 예정
+// 필요시 유효성 검사 관련 import 추가 예정
 
 // 면접 가능 일시를 선택하고 제출하면(한 번만 가능), 애플리케이션 테이블 해당 프로젝트 projectId에
 // 지원자 id, 지원자가 선택한 resumeId, status, interviewCode를 해당 프로젝트의 날짜시간값을 가지는 schedule 컬럼이
@@ -10,52 +10,47 @@ const { Project, ProjectSkill, ProjectPhoto, Application, Resume, sequelize } = 
 
 // 추후 지원자측의 스케쥴 수정은 patch로 반영할 수 있을듯!
 
+// 면접 예약
+
 router.put("/:projectId/:applicationId", authMiddleware, async (req, res) => {
     if (!res.locals.user) {
       res.status(401).json({ errorMessage: "로그인 후 사용하세요." });
     } else {
       const { id } = res.locals.user;
       const { projectId, applicationId } = req.params;
-      console.log(projectId)
-
-    // 유효성 검사 추후 추가
-    //   try {
-    //     var { title, details, subscript, role, start, end, skills, email, phone, schedule, photos }
-    //     = await projectPostSchema.validateAsync(req.body);
-    //   } catch (err) {
-    //     return res.status(400).json({ errorMessage: "작성 형식을 확인해주세요." });
-    //   }
-
       const { resumeId } = req.body;
       const status = 'reserved' // => '지원서 접수' 단계에 해당
-      const interviewCode = 123456 // 6자리 유니크 난수
+      const interviewCode = '765432' // 6자리 유니크 난수
 
       const existProject = await Project.findOne({
         where: { projectId } 
       });
-
+      const alreadyApplied = await Application.findOne({
+        where: { projectId, id }
+      });
       const existApplications = await Application.findOne({
         where: { projectId, applicationId }
       });
 
-      try { if (existApplications.status === null) {};
-      } catch(error) {
-        res.status(400).send({ errorMessage : '이미 예약된 시간대입니다.' });
-      }
-
-      try { if (existApplications.available === true) {};
-      } catch(error) {
-        res.status(400).send({ errorMessage : '예약할 수 없는 시간대입니다.' });
-      }
-
-      try { if (id !== existProject.id) {};
-      } catch(error) {
-        res.status(403).send({ errorMessage : '자신의 프로젝트에는 지원할 수 없습니다.' });
-      }  
-
-      Application.update({ id, resumeId, status, interviewCode },
-      { where: { projectId, applicationId } });
-
+      if (alreadyApplied) {
+        res.status(400).send({ errorMessage : '이미 해당 프로젝트에 지원하셨습니다.' });
+      } else {
+        if (existApplications.status !== null) {
+          res.status(400).send({ errorMessage : '이미 예약된 시간대입니다.' });
+        } else {
+          if (existApplications.available !== true) {
+            return res.status(400).send({ errorMessage : '예약할 수 없는 시간대입니다.' });
+          } else {
+            if (id === existProject.id) {
+              return res.status(403).send({ errorMessage : '자신의 프로젝트에는 지원할 수 없습니다.' });
+            } else {
+              await Application.update({ id, resumeId, status, interviewCode, available: false },
+              { where: { projectId, applicationId } });
+              res.status(200).send({ message : '성공적으로 예약되었습니다.' })
+            };
+          };
+        };
+      };
     };
   });
 
