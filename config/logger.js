40431100b7602fef9eda4762c420b4bfbe6bf34f
engin9803 <mne9691@gmail.com>
2022-07-14 23:52:38 +0900

    Feat : Pm2 로그 생성 경로 기능 추가 Docs : log 기록 상세히 출력 설정

--- config/logger.js
+++ config/logger.js
@@ -58,7 +58,7 @@ const logger = winston.createLogger({
   ],
 });
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
       format: winston.format.combine(
