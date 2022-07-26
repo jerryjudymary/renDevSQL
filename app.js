210f66c48092bd284a5152e96d790846420abe4a
engin9803 <mne9691@gmail.com>
2022-07-26 17:13:33 +0900

    Docs : port -> 3000

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require("./index.js");
 const logger = require("./config/logger");
-const port = 5000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(port, "PORT -=LISTENING=-");
