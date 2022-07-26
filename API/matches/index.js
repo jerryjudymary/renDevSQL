258d393cc3058d9776521c868de9f64646305ee9
engin9803 <mne9691@gmail.com>
2022-07-26 10:27:54 +0900

    Docs : 테스트코드 일부 수정 matches every함수로 변경

--- API/matches/index.js
+++ API/matches/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../../middlewares/authMiddleware");
 const matcheController = require("./controllers/matches.controller");
 
 // "Resume의 조건에 맞는 Projects" 매칭 API
-router.get("/projects/:resumeId", matcheController.projectMatches);
+router.get("/projects/:resumeId", authMiddleware, matcheController.projectMatches);
 
 // "Project의 조건에 맞는 Resumes" 매칭 API
-router.get("/resumes/:projectId", matcheController.resumeMatches);
+router.get("/resumes/:projectId", authMiddleware, matcheController.resumeMatches);
 
 module.exports = router;
