21a9623664ca5d738111a723bc068fb534df22ff
engin9803 <mne9691@gmail.com>
2022-07-26 16:51:23 +0900

    Docs : app.js Port 5000 변경 orgin -> localhost:3000 변경

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require("./index.js");
 const logger = require("./config/logger");
-const port = 3000;
+const port = 5000;
 
 app.listen(port, () => {
   console.log(port, "PORT -=LISTENING=-");
--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const Router = require("./routes");
 
 app.use(
   cors({
-    origin: true, //["http://localhost:3001"],
+    origin: "localhost:3000", //["http://localhost:3001"],
     credentials: true,
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE'
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
   })
 );
 
